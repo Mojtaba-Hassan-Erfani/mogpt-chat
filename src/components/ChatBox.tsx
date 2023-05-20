d4a34b2488ea94849bd626d26413dbe74df68069
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-05-21 00:35:59 +0200

    Feat: Add textarea functionality
    and markup cleanup

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,134 +1,135 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 const ChatBox = () => {
-   const [ prompt, setPrompt ] = useState( '' );
-   const [ isLoading, setIsLoading ] = useState( false );
-   const [ response, setResponse ] = useState( '' );
 
-   const getResponseFromOpenAi = async () => {
-      setResponse( '' );
-      setIsLoading( true );
+    // OpenAI API handling.
+    // ---------------------------------------------------------------------------------------
+    const [ prompt, setPrompt ] = useState( '' );
+    const [ isLoading, setIsLoading ] = useState( false );
+    const [ response, setResponse ] = useState( '' );
 
-      const response = await fetch( '/api/openai', {
-         method: 'POST',
-         headers: {
-            'Content-Type': 'application/json',
-         },
-         body: JSON.stringify( { prompt: prompt } ),
-      } );
+    const getResponseFromOpenAi = async () => {
+        setResponse( '' );
+        setIsLoading( true );
 
-      const data = await response.json();
-      setIsLoading( false );
-      console.log( data.text );
-      setResponse( data.text );
-   };
+        const response = await fetch( '/api/openai', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify( { prompt: prompt } ),
+        } );
 
-   const handleKeyDown = ( e: React.KeyboardEvent<HTMLTextAreaElement> ) => {
-      if ( e.key === 'Enter' && !e.shiftKey ) {
-         e.preventDefault();
-         getResponseFromOpenAi();
-      }
-   };
+        const data = await response.json();
+        setIsLoading( false );
+        console.log( data.text );
+        setResponse( data.text );
+    };
+    // ---------------------------------------------------------------------------------------
+
+
+    // Textarea functionality.
+    // ---------------------------------------------------------------------------------------
+    const formRef = useRef<HTMLFormElement | null>( null );
+    const textAreaRef = useRef<HTMLTextAreaElement | null>( null );
+
+    useEffect( () => {
+        const textArea = textAreaRef.current;
+        const autoResize = () => {
+            if ( textArea ) {
+                textArea.style.height = 'auto';
+                textArea.style.height = `${textArea.scrollHeight}px`;
+            }
+        };
+
+        if ( textArea && textArea.value.trim() !== "" ) {
+            textArea.addEventListener( 'input', autoResize );
+        }
+
+        // Clean up event listener on unmount, for performance issues and to prevent bugs.
+        return () => {
+            if ( textArea ) {
+            textArea.removeEventListener( 'input', autoResize );
+            }
+        }
+    }, [] );
+
+    // Handle Enter action for Textarea.
+    const handleKeyDown = ( e: React.KeyboardEvent<HTMLTextAreaElement> ) => {
+
+        if ( e.key === 'Enter' && ! e.shiftKey ) {
+            e.preventDefault();
+
+            if ( textAreaRef.current?.value.trim() !== "" ) {
+                getResponseFromOpenAi();
+                formRef.current?.reset(); // TODO: Still not working.
+            }
+        }
+
+    };
+   // ---------------------------------------------------------------------------------------
 
    return (
-      <div className="col-md-8 mx-auto d-flex flex-column row-padding">
-         <div
-            className="card flex-grow-1"
-            style={{ height: 'calc(100vh - 120px)' }}
-         >
-            <div className="card-body d-flex flex-column">
-               <form>
-                  <div className="mb-3 input-group">
-                     <label
-                        htmlFor="chatbox-input"
-                        className="form-label visually-hidden"
-                     >
-                        Enter your message:
-                     </label>
-                     <textarea
-                        className="form-control"
-                        id="chatbox-input"
-                        placeholder="Type your message here..."
-                        style={{
-                           maxHeight: '500px',
-                           paddingRight: '50px',
-                           borderBottom: 'none',
-                           borderRight: 'none',
-                           borderLeft: 'none',
-                        }}
-                        rows={1}
-                        onChange={( e ) => {
-                           setPrompt( e.target.value );
-                        }}
-                        onKeyDown={handleKeyDown}
-                     ></textarea>
-                     <div
-                        className="input-group-append"
-                        style={{
-                           position: 'absolute',
-                           right: '0',
-                           top: '0',
-                           paddingTop: '8px',
-                           paddingRight: '5px',
-                        }}
-                     >
-                        <a type="submit" onClick={getResponseFromOpenAi}>
-                           <i
-                              className="bi bi-chevron-right"
-                              style={{
-                                 height: '12px',
-                                 width: '12px',
-                              }}
-                           ></i>
-                        </a>
-                     </div>
-                  </div>
-               </form>
-               <div
-                  id="chat-messages"
-                  className="chat-messages-container message-text"
-                  style={{ height: 'calc(100vh - 120px)' }}
-               >
-                  {/*  Sample chat messages  */}
-                  {prompt && (
-                     <div className="d-flex justify-content-between mb-3">
-                        <div className="p-2 rounded bg-light">
-                           <p>
-                              <strong>User: </strong> {prompt}
-                           </p>
-                        </div>
-                        <i
-                           className="bi bi-pin-angle"
-                           style={{ fontSize: '1rem' }}
-                        ></i>
-                     </div>
-                  )}
-                  <div className="d-flex mb-3 justify-content-end">
-                     {isLoading ? (
-                        <div
-                           className="spinner-border"
-                           role="status"
-                           style={{ fontSize: '1rem' }}
-                        >
-                           <span className="visually-hidden">Loading...</span>
-                        </div>
-                     ) : (
-                        response && (
-                           <div className="p-2 rounded bot-message">
-                              <p>
-                                 <strong>Moji: </strong>
-                                 {response}
-                              </p>
-                           </div>
-                        )
-                     )}
-                  </div>
+    <div className="chatbox-container col-md-8">
 
-                  {/*  End of chat messages  */}
-               </div>
+        {/*  Prompt request and response messages  */}
+        <div className="prompt-display">
+
+            {prompt && prompt.trim() !== '' && (
+                <div className="d-flex justify-content-between mb-3">
+                    <div className="prompt-req">
+                        <p>
+                            <strong>User: </strong> {prompt}
+                        </p>
+                    </div>
+                    <i className="bi bi-pin-angle"></i>
+                </div>
+            )}
+
+            <div className="d-flex mb-3 justify-content-end">
+
+                {isLoading ? (
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                ) : (
+                response && (
+                    <div className="prompt-res">
+                        <p>
+                            <strong>Moji: </strong>
+                            {response}
+                        </p>
+                    </div>
+                )
+                )}
+
             </div>
-         </div>
-      </div>
+        </div>
+        {/* End Prompt request and response messages  */}
+        <form className="chatbox">
+            <div className="mb-3 input-group">
+                <label htmlFor="prompt-input" className="form-label visually-hidden">
+                    Enter your message:
+                </label>
+                <div className="prompt-input">
+                    <textarea id="prompt-input"
+                    placeholder="Type your prompt here..."
+                    rows={1}
+                    ref={textAreaRef}
+                    onChange={( e ) => {
+                        setPrompt( e.target.value );
+                    }}
+                    onKeyDown={handleKeyDown}
+                    ></textarea>
+                    <div className="send-icon">
+                    <a type="submit" onClick={getResponseFromOpenAi}>
+                        <i className="bi bi-chevron-right"></i>
+                    </a>
+                </div>
+                </div>
+            </div>
+        </form>
+    </div>
    );
 };
 
