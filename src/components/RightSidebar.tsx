b62a774b509daf1758194cd4487d5a2d1a61108f
Mojtaba Hassan ERFANI <erfani.mojtaba@hotmail.com>
2023-05-29 03:45:49 +0200

    Style: Sidebar-right redesign, only static

--- src/components/RightSidebar.tsx
+++ src/components/RightSidebar.tsx
@@ -1,36 +1,59 @@
 const RightSidebar = () => {
     return (
+        <div className="side-bar-right d-flex flex-column vh-100 border-end px-4 py-3 col-md-2">
+            <h5 className="text-muted">Language Model</h5>
+            <span><small className="text-muted">Choose language model...</small></span>
+            <select className="form-select mb-4" aria-label="Language select">
+                <option selected>gpt-3.5-turbo</option>
+                <option value="2">code-davinci-002</option>
+                <option value="3">gpt-4</option>
+            </select>
 
-        <div className="sidebar-right col-md-2 bg-light d-flex flex-column">
-            <ul className="nav d-flex">
-                <li className="nav-item">
-                    <a className="active"  href="#"><i className="bi bi-pin-angle-fill"></i></a>
-                </li>
-                <li className="nav-item">
-                    <a className="" href="#"><i className="bi bi-gear"></i></a>
-                </li>
-                <li className="nav-item">
-                    <a className="" href="#"><i className="bi bi-palette"></i></a>
-                </li>
-            </ul>
-            <div className="flex-grow-1">
-                {/*  titles with chat icons */}
-                <ul className="bookmark">
-                    <li className="d-flex">
-                        <span>Sample Title 1</span>
-                        <div>
-                            <i className="bi bi-pencil"></i>
-                            <i className="bi bi-x-circle"></i>
+            <h5 className="text-muted">Pinned Chats</h5>
+            <div className="pinned-chats overflow-auto">
+                <div className="card mb-1">
+                    <div className="card-body">
+                        <span className="card-title">Chat Title 1</span>
+                        <p className="pinned-desc">This is a description for Chat Title 1.</p>
+                        <div className="text-end pinned-icons">
+                            <a href="#" className="text-decoration-none me-2"><i className="bi bi-pencil-fill"></i></a>
+                            <a href="#" className="text-decoration-none"><i className="bi bi-trash-fill"></i></a>
                         </div>
-                    </li>
-                    <li className="d-flex active">
-                        <span>200 Lorem Ipsum words</span>
-                        <div>
-                            <i className="bi bi-pencil"></i>
-                            <i className="bi bi-x-circle"></i>
+                    </div>
+                </div>
+
+                <div className="card mb-2">
+                    <div className="card-body">
+                        <span className="card-title">Chat Title 2</span>
+                        <p className="card-text pinned-desc">This is a description for Chat Title 2.</p>
+                        <div className="text-end pinned-icons">
+                            <a href="#" className="text-decoration-none me-2"><i className="bi bi-pencil-fill"></i></a>
+                            <a href="#" className="text-decoration-none"><i className="bi bi-trash-fill"></i></a>
                         </div>
-                    </li>
-                </ul>
+                    </div>
+                </div>
+
+                <div className="card mb-2">
+                    <div className="card-body">
+                        <span className="card-title">Chat Title 3</span>
+                        <p className="card-text pinned-desc">This is a description for Chat Title 3.</p>
+                        <div className="text-end pinned-icons">
+                            <a href="#" className="text-decoration-none me-2"><i className="bi bi-pencil-fill"></i></a>
+                            <a href="#" className="text-decoration-none"><i className="bi bi-trash-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card mb-2">
+                    <div className="card-body">
+                        <span className="card-title">Chat Title 4</span>
+                        <p className="card-text pinned-desc">This is a description for Chat Title 4.</p>
+                        <div className="text-end pinned-icons">
+                            <a href="#" className="text-decoration-none me-2"><i className="bi bi-pencil-fill"></i></a>
+                            <a href="#" className="text-decoration-none"><i className="bi bi-trash-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
